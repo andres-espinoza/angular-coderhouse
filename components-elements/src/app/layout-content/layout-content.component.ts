19281a099c6317e2595dedf5e654591fb3817bf1
Andrés Espinoza D <andres.espinozad@mail.udp.cl>
2023-03-19 13:17:09 -0300

    first challenge
    Simple layout with navbar, sidebar and main container

---
+++ components-elements/src/app/layout-content/layout-content.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-layout',
+  templateUrl: './layout-content.component.html',
+  styleUrls: ['./layout-content.component.scss'],
+})
+export class ContentLayoutComponent {}
