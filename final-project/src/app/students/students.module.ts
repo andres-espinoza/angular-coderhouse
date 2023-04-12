4cc840708948a03fe73c6432d108db027f09ed78
Andrés Espinoza D <andres.espinozad@mail.udp.cl>
2023-04-12 11:45:41 -0400

    ts config update to work with JSON

--- final-project/src/app/students/students.module.ts
+++ final-project/src/app/students/students.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentsSectionTitleComponent } from './ui/students-section-title/students-section-title.component';
+import { StudentsLayoutComponent } from './ui/students-layout/students-layout.component';
+import { StudentsTableComponent } from './ui/students-table/students-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { FullNamePipe } from './utils/full-name.pipe';
 
 @NgModule({
-  declarations: [StudentsSectionTitleComponent],
-  imports: [CommonModule],
-  exports: [StudentsSectionTitleComponent],
+  declarations: [
+    StudentsSectionTitleComponent,
+    StudentsLayoutComponent,
+    StudentsTableComponent,
+    FullNamePipe,
+  ],
+  imports: [CommonModule, MatTableModule],
+  exports: [
+    StudentsSectionTitleComponent,
+    StudentsLayoutComponent,
+    StudentsTableComponent,
+  ],
 })
 export class StudentsModule {}
