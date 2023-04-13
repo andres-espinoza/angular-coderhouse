637a0c53c038244e5d0479d90d2fc8558e582f25
Andrés Espinoza D <andres.espinozad@mail.udp.cl>
2023-04-12 22:35:57 -0400

    adding logic to modify registers

--- final-project/src/app/students/ui/students-layout/students-layout.component.ts
+++ final-project/src/app/students/ui/students-layout/students-layout.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import studentsJSON from '../../../../__mock__/students/students.json';
 import { Student } from 'src/types/student';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { StudentDialogResult } from 'src/types/studentDialog';
 
 @Component({
   selector: 'app-students-layout',
@@ -21,10 +22,33 @@ export class StudentsLayoutComponent {
       horizontalPosition: 'center',
     });
   }
+
   addStudent(newStudent: Student) {
     this.studentsData = [...this.studentsData, newStudent];
     this.openSnackBar(
       `${newStudent.firstName} ${newStudent.lastName} ha sido agregado`
     );
+  }
+
+  modifyStudent(result: StudentDialogResult) {
+    const { student, actionType } = result;
+    const studentIndex = this.studentsData.findIndex(
+      (s) => s.id === student.id
+    );
+    const [modifiedStudent] = this.studentsData.splice(studentIndex, 1);
+    if (actionType === 'MODIFY') {
+      this.studentsData = [...this.studentsData, student];
+      this.openSnackBar(
+        `${modifiedStudent.firstName} ${modifiedStudent.lastName} se ha actualizado ✔ con éxito`
+      );
+      return;
+    }
+    if (actionType === 'DELETE') {
+      this.studentsData = [...this.studentsData];
+      this.openSnackBar(
+        `${modifiedStudent.firstName} ${modifiedStudent.lastName} se ha eliminado ✖ con éxito`
+      );
+      return;
+    }
   }
 }
