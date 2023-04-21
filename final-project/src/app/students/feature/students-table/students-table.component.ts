4f0027c6b4043757459127a793e1f1b31b54266a
Andrés Espinoza D <andres.espinozad@mail.udp.cl>
2023-04-20 21:23:51 -0400

    using the StudentService to populate table

--- final-project/src/app/students/feature/students-table/students-table.component.ts
+++ final-project/src/app/students/feature/students-table/students-table.component.ts
@@ -1,44 +1,43 @@
-import {
-  AfterViewInit,
-  Component,
-  EventEmitter,
-  Input,
-  OnChanges,
-  OnInit,
-  Output,
-  SimpleChanges,
-  ViewChild,
-} from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Student } from 'src/types/student';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { StudentDialogComponent } from '../student-dialog/student-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { StudentDialogResult } from 'src/types/studentDialog';
+import { StudentService } from '../../data-access/student.service';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-students-table',
   templateUrl: './students-table.component.html',
   styleUrls: ['./students-table.component.scss'],
 })
-export class StudentsTableComponent
-  implements OnInit, AfterViewInit, OnChanges
-{
-  @Input() data: Student[] = [];
+export class StudentsTableComponent {
   @ViewChild(MatSort, { static: false }) sort!: MatSort;
-  @Output() modifyStudent = new EventEmitter<StudentDialogResult>();
 
-  dataSource: MatTableDataSource<Student>;
+  private dataSource = new MatTableDataSource<Student>();
+
+  matTableDataSource$: Observable<MatTableDataSource<Student>> =
+    this.studentsService.getMockStudentFromSubject().pipe(
+      map((students) => {
+        const dataSource = this.dataSource;
+        dataSource.data = students;
+        this.setDataSourceSort(dataSource);
+        return dataSource;
+      })
+    );
 
   displayedColumns: string[] = ['id', 'fullName', 'age', 'score', 'isTopTen'];
 
-  constructor(private matDialog: MatDialog) {
-    this.dataSource = new MatTableDataSource(this.data);
-  }
+  constructor(
+    private matDialog: MatDialog,
+    private studentsService: StudentService
+  ) {}
 
-  setDataSourceSort() {
-    this.dataSource.sort = this.sort;
-    this.dataSource.sortingDataAccessor = (student, columnName) => {
+  setDataSourceSort(dataSource: MatTableDataSource<Student>) {
+    dataSource.sort = this.sort;
+    dataSource.sortingDataAccessor = (student, columnName) => {
       switch (columnName) {
         case 'fullName':
           return `${student.firstName} ${student.lastName}`;
@@ -55,25 +54,11 @@ export class StudentsTableComponent
       data: row,
     });
     dialogRef.afterClosed().subscribe((result: StudentDialogResult) => {
-      this.modifyStudent.emit(result);
+      this.studentsService.modifyStudentsFromSubject(result);
     });
   }
 
   generateToolTip(row: Student): string {
     return `Modificar el registro de ${row.firstName}`;
-  }
-
-  ngOnInit() {
-    this.dataSource = new MatTableDataSource(this.data);
-  }
-
-  ngAfterViewInit() {
-    this.setDataSourceSort();
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    this.data = changes['data'].currentValue;
-    this.dataSource = new MatTableDataSource(this.data);
-    this.setDataSourceSort();
   }
 }
