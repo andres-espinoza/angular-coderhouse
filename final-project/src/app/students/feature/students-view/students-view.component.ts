1ddabe0a693a568fd65e8ace1a62295c707e1a87
Andrés Espinoza D <andres.espinozad@mail.udp.cl>
2023-04-20 21:22:48 -0400

    mocking a simple initial loading case

--- final-project/src/app/students/feature/students-view/students-view.component.ts
+++ final-project/src/app/students/feature/students-view/students-view.component.ts
@@ -1,29 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { Student } from 'src/types/student';
+import { Component, OnDestroy } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { StudentDialogResult } from 'src/types/studentDialog';
-import { StudentsService } from '../../data-access/students.service';
+import { StudentService } from '../../data-access/student.service';
+import { Subscription, delay, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-students-view',
   templateUrl: './students-view.component.html',
   styleUrls: ['./students-view.component.scss'],
 })
-export class StudentsViewComponent implements OnInit {
-  studentsData: Student[] = [];
+export class StudentsViewComponent implements OnDestroy {
   isLoading = true;
+  studentsSubscription$: Subscription = this.studentsService
+    .getMockStudentFromSubject()
+    .pipe(
+      take(1),
+      delay(2000),
+      tap(() => {
+        this.isLoading = false;
+      })
+    )
+    .subscribe();
 
   constructor(
     private _snackBar: MatSnackBar,
-    private studentsService: StudentsService
+    private studentsService: StudentService
   ) {}
 
-  ngOnInit(): void {
-    this.studentsService.getMockStudents().subscribe({
-      next: (students) => (this.studentsData = students),
-      error: (error) => this.openSnackBar(error),
-      complete: () => (this.isLoading = false),
-    });
+  ngOnDestroy(): void {
+    this.studentsSubscription$.unsubscribe();
   }
 
   openSnackBar(message: string, action?: string) {
@@ -33,34 +37,5 @@ export class StudentsViewComponent implements OnInit {
       verticalPosition: 'bottom',
       horizontalPosition: 'center',
     });
-  }
-
-  addStudent(newStudent: Student) {
-    this.studentsData = [...this.studentsData, newStudent];
-    this.openSnackBar(
-      `${newStudent.firstName} ${newStudent.lastName} ha sido agregado`
-    );
-  }
-
-  modifyStudent(result: StudentDialogResult) {
-    const { student, actionType } = result;
-    const studentIndex = this.studentsData.findIndex(
-      (s) => s.id === student.id
-    );
-    const [modifiedStudent] = this.studentsData.splice(studentIndex, 1);
-    if (actionType === 'MODIFY') {
-      this.studentsData = [...this.studentsData, student];
-      this.openSnackBar(
-        `${modifiedStudent.firstName} ${modifiedStudent.lastName} se ha actualizado ✔ con éxito`
-      );
-      return;
-    }
-    if (actionType === 'DELETE') {
-      this.studentsData = [...this.studentsData];
-      this.openSnackBar(
-        `${modifiedStudent.firstName} ${modifiedStudent.lastName} se ha eliminado ✖ con éxito`
-      );
-      return;
-    }
   }
 }
