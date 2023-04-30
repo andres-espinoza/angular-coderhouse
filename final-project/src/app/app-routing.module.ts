f19036ad03b93023fcf92d3f06a93b5463b2009f
Andrés Espinoza D <andres.espinozad@mail.udp.cl>
2023-04-30 11:58:15 -0400

    adding routes

--- final-project/src/app/app-routing.module.ts
+++ final-project/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentsViewComponent } from './students/feature/students-view/students-view.component';
+import { LoginFormComponent } from './login/feature/login-form/login-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: StudentsViewComponent,
+  },
+  {
+    path: 'login',
+    component: LoginFormComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
